82444ad54e94b92bcb94afa672bc7dc036b37412
Sundar Rajamani <meetsundar23@gmail.com>
2018-10-16 14:00:32 +0530

    added new test cases

---
+++ JenkinSelenium/src/test/java/E2E/JenkinSelenium/TC3.java
@@ -0,0 +1,25 @@
+package E2E.JenkinSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC3 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys("uname1");
+		
+		driver.findElement(By.id("pass")).sendKeys("pass1");
+		driver.quit();
+	}
+
+}
